f37adcd453469cde4583e08321d0954313329e2a
paulobressan <paulo.bressan@outlook.com>
2023-09-25 16:34:13 -0300

    chore(chain/update): removed do_work function example

--- src/chain/update.rs
+++ src/chain/update.rs
@@ -11,11 +11,6 @@ pub struct Args {
     name: String,
 }
 
-#[instrument]
-async fn do_work(slot: u64) {
-    thread::sleep(Duration::from_millis(12));
-}
-
 #[instrument("update", skip_all)]
 pub async fn run(args: Args) -> miette::Result<()> {
     info!(chain = args.name, "updating");
@@ -23,27 +18,28 @@ pub async fn run(args: Args) -> miette::Result<()> {
     let mut slot = 0;
     let slot_tip = 500;
 
-    let header_span = info_span!("chain-update");
-    header_span.pb_set_style(&ProgressStyle::default_bar());
-    header_span.pb_set_length(slot_tip);
+    let span = info_span!("chain-update");
+    span.pb_set_style(&ProgressStyle::default_bar());
+    span.pb_set_length(slot_tip);
 
-    header_span.pb_set_style(
+    span.pb_set_style(
         &ProgressStyle::with_template(
             "{spinner:.white} [{elapsed_precise}] [{wide_bar:.white/white}] {pos}/{len}",
         )
         .unwrap(),
     );
 
-    let header_span_enter = header_span.enter();
+    let span_enter = span.enter();
 
     while slot < slot_tip {
         slot += 1;
-        do_work(slot).await;
+
+        thread::sleep(Duration::from_millis(12));
         Span::current().pb_inc(1);
     }
 
-    std::mem::drop(header_span_enter);
-    std::mem::drop(header_span);
+    std::mem::drop(span_enter);
+    std::mem::drop(span);
 
     info!("chain updated");
 
