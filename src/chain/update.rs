e637d8b8caa2a473429bc05f65d22da054ad20da
paulobressan <paulo.bressan@outlook.com>
2023-09-25 15:37:46 -0300

    feat(chain/update): added sync progress bar

--- src/chain/update.rs
+++ src/chain/update.rs
@@ -1,5 +1,9 @@
+use std::{fmt::Write, thread, time::Duration};
+
 use clap::Parser;
-use tracing::{info, instrument};
+use indicatif::{ProgressState, ProgressStyle};
+use tracing::{info, info_span, instrument, Span};
+use tracing_indicatif::span_ext::IndicatifSpanExt;
 
 #[derive(Parser)]
 pub struct Args {
@@ -7,14 +11,39 @@ pub struct Args {
     name: String,
 }
 
+#[instrument]
+async fn do_work(slot: u64) {
+    thread::sleep(Duration::from_millis(12));
+}
+
 #[instrument("update", skip_all)]
 pub async fn run(args: Args) -> miette::Result<()> {
     info!(chain = args.name, "updating");
 
-    for i in 0..3 {
-        info!(slot = i, "update");
-        tokio::time::sleep(std::time::Duration::from_secs(3)).await;
+    let mut slot = 0;
+    let slot_tip = 500;
+
+    let header_span = info_span!("header");
+    header_span.pb_set_style(&ProgressStyle::default_bar());
+    header_span.pb_set_length(slot_tip);
+
+    header_span.pb_set_style(
+        &ProgressStyle::with_template(
+            "{spinner:.white} [{elapsed_precise}] [{wide_bar:.white/white}] {pos}/{len}",
+        )
+        .unwrap(),
+    );
+
+    let header_span_enter = header_span.enter();
+
+    while slot < slot_tip {
+        slot += 1;
+        do_work(slot).await;
+        Span::current().pb_inc(1);
     }
+
+    std::mem::drop(header_span_enter);
+    std::mem::drop(header_span);
 
     info!("chain updated");
 
