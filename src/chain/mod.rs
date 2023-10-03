56f35a38e332879ddfe29d92c8933612fef24a95
paulobressan <paulo.bressan@outlook.com>
2023-10-03 11:33:18 -0300

    chore(chain/delete): adjusted tracing

--- src/chain/mod.rs
+++ src/chain/mod.rs
@@ -34,10 +34,7 @@ pub async fn run(args: Args, ctx: &crate::Context) -> miette::Result<()> {
             create::run(args, ctx).await
         }
         Commands::List(args) => list::run(args).await,
-        Commands::Delete(args) => {
-            crate::with_tracing();
-            delete::run(args, ctx).await
-        }
+        Commands::Delete(args) => delete::run(args, ctx).await,
         Commands::Update(args) => {
             crate::with_tracing();
             update::run(args).await
