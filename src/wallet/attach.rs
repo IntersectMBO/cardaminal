7cf752e48ab1f23af5d216f0787cdeb0eb5ebc17
paulobressan <paulo.bressan@outlook.com>
2023-10-12 04:04:06 -0300

    feat: added attach chain to wallet

--- src/wallet/attach.rs
+++ src/wallet/attach.rs
@@ -1,5 +1,8 @@
 use clap::Parser;
+use miette::bail;
 use tracing::{info, instrument};
+
+use crate::{chain::config::Chain, wallet::config::Wallet};
 
 #[derive(Parser)]
 pub struct Args {
@@ -10,7 +13,26 @@ pub struct Args {
 }
 
 #[instrument("attach", skip_all)]
-pub async fn run(args: Args) -> miette::Result<()> {
+pub async fn run(args: Args, ctx: &crate::Context) -> miette::Result<()> {
+    let wallet = Wallet::load_config(&ctx.dirs.root_dir, &args.wallet)?;
+    if wallet.is_none() {
+        bail!("wallet doesn't exist")
+    }
+
+    if !Chain::dir(&ctx.dirs.root_dir, &args.chain).exists() {
+        bail!("chain doesn't exist")
+    }
+
+    let mut wallet = wallet.unwrap();
+    // TODO: to enable the option to replace the chain, it's necessary to validate if already exists data from the old chain
+    if wallet.chain.is_some() {
+        bail!("wallet already attached")
+    }
+
+    wallet.chain = Some(args.chain.clone());
+
+    wallet.save_config(&ctx.dirs.root_dir)?;
+
     info!(wallet = args.wallet, chain = args.chain, "attached",);
     Ok(())
 }
