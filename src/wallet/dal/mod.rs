a25bd9f0eb1338e875fe4ea93defa9d3f4242a9c
jmhrpr <harper.jme@gmail.com>
2023-10-15 19:37:14 +0100

    feat: initial walletDB helpers for required storage actions

--- src/wallet/dal/mod.rs
+++ src/wallet/dal/mod.rs
@@ -1,17 +1,19 @@
 pub mod entities;
-pub mod migration; // TODO
+pub mod migration;
 
 use std::path::PathBuf;
 
 use pallas::ledger::addresses::{Address, ShelleyPaymentPart};
 use pallas::ledger::traverse::{MultiEraInput, MultiEraOutput};
 use sea_orm::entity::prelude::*;
-use sea_orm::{Condition, Database, TransactionTrait};
+use sea_orm::{Condition, Database, Order, Paginator, QueryOrder, SelectModel, TransactionTrait};
 use sea_orm_migration::MigratorTrait;
 
-use self::entities::prelude::Utxo;
-use self::entities::utxo;
+use self::entities::prelude::{ProtocolParameters, RecentPoints, TxHistory, Utxo};
+use self::entities::{protocol_parameters, recent_points, tx_history, utxo};
 use self::migration::Migrator;
+
+static DEFAULT_PAGE_SIZE: u64 = 20;
 
 pub struct WalletDB {
     pub name: String,
@@ -33,6 +35,8 @@ impl WalletDB {
     pub async fn migrate_up(&self) -> Result<(), DbErr> {
         Migrator::up(&self.conn, None).await
     }
+
+    // UTxOs
 
     pub async fn insert_utxos(
         &self,
@@ -101,44 +105,174 @@ impl WalletDB {
         txn.commit().await
     }
 
-    pub async fn fetch_utxos_for_payment_cred(
+    pub fn paginate_utxos(
         &self,
-        cred: [u8; 28],
-    ) -> Result<Vec<utxo::Model>, DbErr> {
+        order: Order,
+        page_size: Option<u64>,
+    ) -> Paginator<'_, DatabaseConnection, SelectModel<utxo::Model>> {
         Utxo::find()
-            .filter(utxo::Column::PaymentCred.eq(cred.to_vec()))
-            .all(&self.conn)
-            .await
+            .order_by(tx_history::Column::Slot, order.clone())
+            .paginate(&self.conn, page_size.unwrap_or(DEFAULT_PAGE_SIZE))
     }
 
-    pub async fn fetch_utxos_for_address(
+    pub async fn paginate_utxos_for_address(
         &self,
         address: Address,
-    ) -> Result<Vec<utxo::Model>, DbErr> {
+        order: Order,
+        page_size: Option<u64>,
+    ) -> Paginator<'_, DatabaseConnection, SelectModel<utxo::Model>> {
         Utxo::find()
             .filter(utxo::Column::FullAddress.eq(address.to_vec()))
-            .all(&self.conn)
-            .await
+            .order_by(tx_history::Column::Slot, order.clone())
+            .paginate(&self.conn, page_size.unwrap_or(DEFAULT_PAGE_SIZE))
     }
 
-    // // TODO: balance type
-    // pub async fn insert_history_tx(&self, tx_hash: [u8; 32], slot: u64, tx_block_index: u16, delta: Vec<u8> ) -> {
-    //     ()
-    // }
+    // Transaction History
 
-    // pub async fn fetch_history_for_payment_cred(&self, cred: [u8; 28]) -> () {
-    //     ()
-    // }
+    // TODO: balance type
+    pub async fn insert_history_tx(
+        &self,
+        tx_hash: [u8; 32],
+        slot: u64,
+        tx_block_index: u16,
+        delta: Vec<u8>,
+    ) -> Result<(), DbErr> {
+        let history_model = entities::tx_history::ActiveModel {
+            tx_hash: sea_orm::ActiveValue::Set(tx_hash.to_vec()),
+            slot: sea_orm::ActiveValue::Set(slot as i64),
+            block_index: sea_orm::ActiveValue::Set(tx_block_index.into()),
+            balance_delta: sea_orm::ActiveValue::Set(delta),
+            ..Default::default()
+        };
 
-    // pub async fn insert_recent_point(&self, slot: u64, block_hash: [u8; 32]) -> () {
-    //     ()
-    // }
+        let _ = TxHistory::insert(history_model).exec(&self.conn).await?;
 
-    // pub async fn remove_recent_points_after_slot(&self, slot: u64) -> () {
-    //     ()
-    // }
+        Ok(())
+    }
 
-    // pub async fn remove_recent_points_before_slot(&self, slot: u64) -> () {
-    //     ()
-    // }
+    pub fn paginate_tx_history(
+        &self,
+        order: Order,
+        page_size: Option<u64>,
+    ) -> Paginator<'_, DatabaseConnection, SelectModel<tx_history::Model>> {
+        TxHistory::find()
+            .order_by(tx_history::Column::Slot, order.clone())
+            .order_by(tx_history::Column::BlockIndex, order)
+            .paginate(&self.conn, page_size.unwrap_or(DEFAULT_PAGE_SIZE))
+    }
+
+    // Recent Points
+
+    pub async fn insert_recent_point(&self, slot: u64, block_hash: [u8; 32]) -> Result<(), DbErr> {
+        let point_model = entities::recent_points::ActiveModel {
+            slot: sea_orm::ActiveValue::Set(slot as i64),
+            block_hash: sea_orm::ActiveValue::Set(block_hash.into()),
+            ..Default::default()
+        };
+
+        let _ = RecentPoints::insert(point_model).exec(&self.conn).await?;
+
+        Ok(())
+    }
+
+    pub async fn remove_recent_points_before_slot(&self, slot: u64) -> Result<(), DbErr> {
+        let txn = self.conn.begin().await?;
+
+        let point_models = RecentPoints::find()
+            .filter(Condition::all().add(recent_points::Column::Slot.lt(slot)))
+            .all(&txn)
+            .await?;
+
+        for point_model in point_models {
+            let _ = point_model.delete(&txn).await?;
+        }
+
+        txn.commit().await
+    }
+
+    // Protocol Parameters
+
+    pub async fn insert_protocol_parameters(
+        &self,
+        slot: u64,
+        tx_block_index: u16,
+        update_cbor: Vec<u8>,
+    ) -> Result<(), DbErr> {
+        let pparams_model = entities::protocol_parameters::ActiveModel {
+            slot: sea_orm::ActiveValue::Set(slot as i64),
+            block_index: sea_orm::ActiveValue::Set(tx_block_index.into()),
+            update_cbor: sea_orm::ActiveValue::Set(update_cbor),
+            ..Default::default()
+        };
+
+        let _ = ProtocolParameters::insert(pparams_model)
+            .exec(&self.conn)
+            .await?;
+
+        Ok(())
+    }
+
+    /// Fetch the CBOR of the most recent protocol parameters seen on-chain
+    pub async fn fetch_latest_protocol_parameters(&self) -> Result<Option<Vec<u8>>, DbErr> {
+        let res = ProtocolParameters::find()
+            .order_by_desc(protocol_parameters::Column::Slot)
+            .order_by_desc(protocol_parameters::Column::BlockIndex)
+            .one(&self.conn)
+            .await?;
+
+        Ok(res.map(|r| r.update_cbor))
+    }
+
+    // Rollback
+
+    /// Remove all records from WalletDB created for slots after the specified slot
+    pub async fn rollback_to_slot(&self, slot: u64) -> Result<(), DbErr> {
+        let txn = self.conn.begin().await?;
+
+        // UTxOs
+
+        let point_models = RecentPoints::find()
+            .filter(Condition::all().add(recent_points::Column::Slot.gt(slot)))
+            .all(&txn)
+            .await?;
+
+        for point_model in point_models {
+            let _ = point_model.delete(&txn).await?;
+        }
+
+        // Transaction History
+
+        let tx_models = TxHistory::find()
+            .filter(Condition::all().add(tx_history::Column::Slot.gt(slot)))
+            .all(&txn)
+            .await?;
+
+        for tx_model in tx_models {
+            let _ = tx_model.delete(&txn).await?;
+        }
+
+        // Recent Points
+
+        let points_models = RecentPoints::find()
+            .filter(Condition::all().add(recent_points::Column::Slot.gt(slot)))
+            .all(&txn)
+            .await?;
+
+        for point_model in points_models {
+            let _ = point_model.delete(&txn).await?;
+        }
+
+        // Protocol Parameters
+
+        let pparams_models = ProtocolParameters::find()
+            .filter(Condition::all().add(protocol_parameters::Column::Slot.gt(slot)))
+            .all(&txn)
+            .await?;
+
+        for pparams_model in pparams_models {
+            let _ = pparams_model.delete(&txn).await?;
+        }
+
+        Ok(())
+    }
 }
