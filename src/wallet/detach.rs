dd48a30906f301421a029439e15f29dc55637518
Paulo Bressan <paulo.bressan@outlook.com>
2023-10-17 12:43:26 -0300

    feat: implement wallet detach command (#53)

--- src/wallet/detach.rs
+++ src/wallet/detach.rs
@@ -1,5 +1,8 @@
 use clap::Parser;
+use miette::{bail, IntoDiagnostic};
 use tracing::{info, instrument};
+
+use crate::wallet::config::Wallet;
 
 #[derive(Parser)]
 pub struct Args {
@@ -8,7 +11,32 @@ pub struct Args {
 }
 
 #[instrument("detach", skip_all)]
-pub async fn run(args: Args) -> miette::Result<()> {
-    info!(wallet = args.wallet, "detached");
+pub async fn run(args: Args, ctx: &crate::Context) -> miette::Result<()> {
+    let wallet = Wallet::load_config(&ctx.dirs.root_dir, &args.wallet)?;
+    if wallet.is_none() {
+        bail!("wallet doesn't exist")
+    }
+
+    let mut wallet = wallet.unwrap();
+    if wallet.chain.is_none() {
+        bail!("wallet hasn't been attached yet")
+    }
+
+    let confirm = inquire::Confirm::new(&format!(
+        "Do you confirm detaching chain {} from the wallet {}?",
+        wallet.chain.unwrap(),
+        &wallet.name
+    ))
+    .prompt()
+    .into_diagnostic()?;
+
+    if confirm {
+        // TODO: check if it's need necessary to delete all data from the old chain from this wallet (sqlite)
+        wallet.chain = None;
+        wallet.save_config(&ctx.dirs.root_dir)?;
+
+        info!(wallet = args.wallet, "detached");
+    }
+
     Ok(())
 }
