4d7d362c5c9502d1fe88c4f7b2738c0017d4e22a
paulobressan <paulo.bressan@outlook.com>
2023-10-19 18:29:04 -0300

    feat: added list utxo

--- src/wallet/config/mod.rs
+++ src/wallet/config/mod.rs
@@ -7,9 +7,10 @@ use std::{
 use chrono::{DateTime, Local};
 use comfy_table::Table;
 use miette::{Context, IntoDiagnostic};
+use pallas::ledger::traverse::{Era, MultiEraOutput};
 use serde::{Deserialize, Serialize};
 
-use super::create::Args;
+use super::{create::Args, dal::entities::prelude::UtxoModel};
 use crate::utils::{deserialize_date, serialize_date, OutputFormatter};
 
 #[derive(Debug, Serialize, Deserialize)]
@@ -111,5 +112,83 @@ impl OutputFormatter for Vec<Wallet> {
     fn to_json(&self) {
         let json = serde_json::to_string_pretty(self).unwrap();
         println!("{json}");
+    }
+}
+
+#[derive(Debug, Serialize)]
+pub struct UtxoView {
+    pub lovelace: u64,
+    pub datum: bool,
+    pub tokens: Vec<(String, u64)>,
+}
+impl OutputFormatter for Vec<UtxoView> {
+    fn to_table(&self) {
+        let mut table = Table::new();
+
+        table.set_header(vec!["amount", "datum", "tokens"]);
+
+        for utxo in self {
+            let tokens = utxo
+                .tokens
+                .iter()
+                .map(|t| format!("{} {}", t.1, t.0))
+                .collect::<Vec<String>>()
+                .join("\n");
+
+            table.add_row(vec![
+                &utxo.lovelace.to_string(),
+                &utxo.datum.to_string(),
+                &tokens,
+            ]);
+        }
+
+        println!("{table}");
+    }
+
+    fn to_json(&self) {
+        let json = serde_json::to_string_pretty(self).unwrap();
+        println!("{json}");
+    }
+}
+impl TryFrom<UtxoModel> for UtxoView {
+    type Error = miette::ErrReport;
+
+    fn try_from(value: UtxoModel) -> Result<Self, Self::Error> {
+        // TODO validate if is possible extract this code
+        let era = match value.era {
+            0 => Era::Byron,
+            1 => Era::Alonzo,
+            3 => Era::Babbage,
+            _ => unreachable!("unexpected era"),
+        };
+
+        let output = MultiEraOutput::decode(era, &value.cbor).into_diagnostic()?;
+
+        let lovelace = output.lovelace_amount();
+        let datum: bool = output.datum().is_some();
+        let tokens: Vec<(String, u64)> = output
+            .non_ada_assets()
+            .iter()
+            .map(|p| {
+                p.assets()
+                    .iter()
+                    .map(|a| {
+                        (
+                            a.to_ascii_name().unwrap_or_default(),
+                            a.output_coin().unwrap_or_default(),
+                        )
+                    })
+                    .collect::<Vec<(String, u64)>>()
+            })
+            .flatten()
+            .collect();
+
+        let utxo_view = UtxoView {
+            lovelace,
+            datum,
+            tokens,
+        };
+
+        Ok(utxo_view)
     }
 }
