be8fbd7744dc8ddee2d93505ae2194352983f962
paulobressan <paulo.bressan@outlook.com>
2023-10-26 17:01:20 -0300

    feat(transaction/output): added args to add, remove command

--- src/transaction/output/remove.rs
+++ src/transaction/output/remove.rs
@@ -2,7 +2,12 @@ use clap::Parser;
 use tracing::instrument;
 
 #[derive(Parser)]
-pub struct Args {}
+pub struct Args {
+    /// transaction id
+    tx_id: String,
+    /// output index
+    output_index: u16,
+}
 
 #[instrument("remove", skip_all, fields())]
 pub async fn run(_args: Args) -> miette::Result<()> {
