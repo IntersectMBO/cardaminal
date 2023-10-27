6e3c54a4ce8cc91a97326709ee4eecfaa84b737e
paulobressan <paulo.bressan@outlook.com>
2023-10-27 11:46:52 -0300

    feat(transaction/script): added args to add, remove command

--- src/transaction/script/add.rs
+++ src/transaction/script/add.rs
@@ -2,7 +2,17 @@ use clap::Parser;
 use tracing::instrument;
 
 #[derive(Parser)]
-pub struct Args {}
+pub struct Args {
+    /// transaction id
+    tx_id: String,
+
+    /// script bytes
+    #[arg(long, short, action)]
+    bytes: Option<Vec<u8>>,
+    /// script file path
+    #[arg(long, short, action)]
+    file: Option<String>,
+}
 
 #[instrument("add", skip_all, fields())]
 pub async fn run(_args: Args) -> miette::Result<()> {
