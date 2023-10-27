aeb29143f3e927023e8904d791842040eb107ea3
paulobressan <paulo.bressan@outlook.com>
2023-10-27 11:28:42 -0300

    feat(transaction/mint): added args to add, remove command

--- src/transaction/mint/remove.rs
+++ src/transaction/mint/remove.rs
@@ -2,7 +2,12 @@ use clap::Parser;
 use tracing::instrument;
 
 #[derive(Parser)]
-pub struct Args {}
+pub struct Args {
+    /// transaction id
+    tx_id: String,
+    /// mint asset [policy][name]
+    asset: String,
+}
 
 #[instrument("remove", skip_all, fields())]
 pub async fn run(_args: Args) -> miette::Result<()> {
