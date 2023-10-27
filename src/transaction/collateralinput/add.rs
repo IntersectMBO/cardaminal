8ef29f955c83c9073aa8fd689319839b295591cb
paulobressan <paulo.bressan@outlook.com>
2023-10-27 11:36:17 -0300

    feat(transaction/collateral-input): added args to add, remove command

--- src/transaction/collateralinput/add.rs
+++ src/transaction/collateralinput/add.rs
@@ -2,7 +2,14 @@ use clap::Parser;
 use tracing::instrument;
 
 #[derive(Parser)]
-pub struct Args {}
+pub struct Args {
+    /// transaction id
+    tx_id: String,
+    /// utxo hash
+    utxo_hash: String,
+    /// utxo idx
+    utxo_idx: String,
+}
 
 #[instrument("add", skip_all, fields())]
 pub async fn run(_args: Args) -> miette::Result<()> {
