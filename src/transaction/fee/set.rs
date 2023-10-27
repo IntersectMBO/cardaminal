95665e7c953589f97373943f20fe911e0308f04f
paulobressan <paulo.bressan@outlook.com>
2023-10-27 11:25:36 -0300

    feat(transaction/fee): added args to set, clear command

--- src/transaction/fee/set.rs
+++ src/transaction/fee/set.rs
@@ -2,7 +2,12 @@ use clap::Parser;
 use tracing::instrument;
 
 #[derive(Parser)]
-pub struct Args {}
+pub struct Args {
+    /// transaction id
+    tx_id: String,
+    /// fee in lovelace
+    lovelace: u64,
+}
 
 #[instrument("set", skip_all, fields())]
 pub async fn run(_args: Args) -> miette::Result<()> {
