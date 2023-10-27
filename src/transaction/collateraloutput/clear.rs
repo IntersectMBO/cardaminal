3e93feffbf948adf35681812769b5c4ba7c0c845
paulobressan <paulo.bressan@outlook.com>
2023-10-27 11:38:20 -0300

    feat(transaction/collateral-output): added args to set, clear command

--- src/transaction/collateraloutput/clear.rs
+++ src/transaction/collateraloutput/clear.rs
@@ -2,7 +2,10 @@ use clap::Parser;
 use tracing::instrument;
 
 #[derive(Parser)]
-pub struct Args {}
+pub struct Args {
+    /// transaction id
+    tx_id: String,
+}
 
 #[instrument("clear", skip_all, fields())]
 pub async fn run(_args: Args) -> miette::Result<()> {
--- src/transaction/collateraloutput/set.rs
+++ src/transaction/collateraloutput/set.rs
@@ -2,7 +2,14 @@ use clap::Parser;
 use tracing::instrument;
 
 #[derive(Parser)]
-pub struct Args {}
+pub struct Args {
+    /// transaction id
+    tx_id: String,
+    /// address to output collateral
+    address: String,
+    /// collateral lovelace
+    lovelace: u64,
+}
 
 #[instrument("set", skip_all, fields())]
 pub async fn run(_args: Args) -> miette::Result<()> {
