928f8d862f664d0b144b5fd3a3094d596cebc6b2
paulobressan <paulo.bressan@outlook.com>
2023-10-27 11:31:33 -0300

    feat(transaction/valid-hereafter): added args to set, clear command

--- src/transaction/validhereafter/set.rs
+++ src/transaction/validhereafter/set.rs
@@ -2,7 +2,12 @@ use clap::Parser;
 use tracing::instrument;
 
 #[derive(Parser)]
-pub struct Args {}
+pub struct Args {
+    /// transaction id
+    tx_id: String,
+    /// slot to validate
+    slot: u64,
+}
 
 #[instrument("set", skip_all, fields())]
 pub async fn run(_args: Args) -> miette::Result<()> {
