9a631fcaca395ffff4a0fb5d067d64aeeff609a2
paulobressan <paulo.bressan@outlook.com>
2023-10-27 11:48:07 -0300

    feat(transaction/datum): added args to add, remove command

--- src/transaction/datum/add.rs
+++ src/transaction/datum/add.rs
@@ -2,7 +2,17 @@ use clap::Parser;
 use tracing::instrument;
 
 #[derive(Parser)]
-pub struct Args {}
+pub struct Args {
+    /// transaction id
+    tx_id: String,
+
+    /// datum bytes
+    #[arg(long, short, action)]
+    bytes: Option<Vec<u8>>,
+    /// datum file path
+    #[arg(long, short, action)]
+    file: Option<String>,
+}
 
 #[instrument("add", skip_all, fields())]
 pub async fn run(_args: Args) -> miette::Result<()> {
--- src/transaction/datum/remove.rs
+++ src/transaction/datum/remove.rs
@@ -2,7 +2,12 @@ use clap::Parser;
 use tracing::instrument;
 
 #[derive(Parser)]
-pub struct Args {}
+pub struct Args {
+    /// transaction id
+    tx_id: String,
+    /// datum hash
+    datum_hash: String,
+}
 
 #[instrument("remove", skip_all, fields())]
 pub async fn run(_args: Args) -> miette::Result<()> {
