8a117aacc1a46f6cdc0d459c19fd746081519510
Santiago Carmuega <santiago@carmuega.me>
2023-11-05 12:36:53 -0300

    fix: show failures in list commands (#91)

--- src/chain/list.rs
+++ src/chain/list.rs
@@ -5,15 +5,23 @@ use tracing::instrument;
 use crate::{chain::config::Chain, utils::OutputFormatter, OutputFormat};
 
 #[derive(Parser)]
-pub struct Args {}
+pub struct Args {
+    #[arg(long, default_value = "false")]
+    ignore_errors: bool,
+}
 
 #[instrument("list", skip_all)]
-pub async fn run(_args: Args, ctx: &crate::Context) -> miette::Result<()> {
-    let chains: Vec<Chain> = Chain::list_available(&ctx.dirs.root_dir)
+pub async fn run(args: Args, ctx: &crate::Context) -> miette::Result<()> {
+    let chains: Vec<_> = Chain::list_available(&ctx.dirs.root_dir)
         .context("error listing chains")?
         .into_iter()
-        .map(|name| Chain::load_config(&ctx.dirs.root_dir, &name))
-        .filter_map(|cfg| cfg.ok().flatten())
+        .map(|name| {
+            Chain::load_config(&ctx.dirs.root_dir, &name).context(format!("loading chain {name}"))
+        })
+        .filter(|cfg| cfg.is_ok() || !args.ignore_errors)
+        .collect::<Result<Vec<_>, _>>()?
+        .into_iter()
+        .flatten()
         .collect();
 
     match ctx.output_format {
