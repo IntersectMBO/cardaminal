3649606a3cb1f35f44edfed202a61705b8b2ca34
Santiago Carmuega <santiago@carmuega.me>
2023-11-06 13:23:56 -0300

    feat: implement add-mint command (#95)

--- src/transaction/edit/mod.rs
+++ src/transaction/edit/mod.rs
@@ -151,7 +151,7 @@ pub async fn run(args: Args, ctx: &crate::Context) -> miette::Result<()> {
         Commands::RemoveOutput(args) => remove_output::run(args, ctx).await,
         Commands::SetFee(args) => set_fee::run(args, &edit_ctx).await,
         Commands::ClearFee(args) => clear_fee::run(args).await,
-        Commands::AddMint(args) => add_mint::run(args).await,
+        Commands::AddMint(args) => add_mint::run(args, &edit_ctx).await,
         Commands::RemoveMint(args) => remove_mint::run(args).await,
         Commands::SetTtl(args) => set_ttl::run(args).await,
         Commands::ClearTtl(args) => clear_ttl::run(args).await,
