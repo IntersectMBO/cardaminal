eb5eee7253d5a69a0beba535f1cad37ad2bcb267
jmhrpr <harper.jme@gmail.com>
2023-11-27 14:46:07 +0000

    feat: add/remove computed signatures

--- src/transaction/edit/add_signature.rs
+++ src/transaction/edit/add_signature.rs
@@ -1,43 +1,75 @@
 use clap::Parser;
-// use miette::{Context, IntoDiagnostic};
-use tracing::instrument;
+use miette::{bail, miette, Context, IntoDiagnostic};
+use pallas::txbuilder::BuiltTransaction;
+use tracing::{info, instrument};
 
-// use super::common::with_staging_tx;
+use crate::wallet::{
+    config::Wallet,
+    dal::{entities::transaction::Status, WalletDB},
+};
 
 #[derive(Parser)]
 pub struct Args {
-    /// hex public key hash
+    /// hex public key (not hash of key)
     public_key: String,
     /// hex signature for a witness from the transaction
     signature: String,
 }
 
 #[instrument("add signature", skip_all, fields(args))]
-pub async fn run(_args: Args, _ctx: &super::EditContext<'_>) -> miette::Result<()> {
-    // TODO: This should operate on a BuiltTransaction not a StagingTransaction
-    todo!()
+pub async fn run(args: Args, ctx: &super::EditContext<'_>) -> miette::Result<()> {
+    let public_key: [u8; 32] = hex::decode(args.public_key)
+        .into_diagnostic()
+        .context("parsing public key hex")?
+        .try_into()
+        .map_err(|_| miette!("public key incorrect length"))?;
 
-    // let public_key: PublicKey = hex::decode(args.public_key)
-    //     .into_diagnostic()
-    //     .context("parsing public key hex")?
-    //     .into();
+    let signature: [u8; 64] = hex::decode(args.signature)
+        .into_diagnostic()
+        .context("parsing signature hex")?
+        .try_into()
+        .map_err(|_| miette!("signature incorrect length"))?;
 
-    // let signature: Signature = hex::decode(args.signature)
-    //     .into_diagnostic()
-    //     .context("parsing signature hex")?
-    //     .into();
+    let wallet = Wallet::load_config(&ctx.global_ctx.dirs.root_dir, &ctx.wallet)?
+        .ok_or(miette::miette!("wallet doesn't exist"))?;
 
-    // TODO: verify if is possible to validate public key and signature
-    // with_staging_tx(ctx, move |mut tx| {
-    // if let Some(signatures) = tx.signatures.as_mut() {
-    //     signatures.insert(public_key, signature);
-    // } else {
-    //     let mut signatures = HashMap::new();
-    //     signatures.insert(public_key, signature);
-    //     tx.signatures = Some(signatures)
-    // }
+    let wallet_db = WalletDB::open(
+        &wallet.name,
+        &Wallet::dir(&ctx.global_ctx.dirs.root_dir, &wallet.name),
+    )
+    .await
+    .into_diagnostic()?;
 
-    // Ok(tx)
-    // })
-    // .await
+    let mut record = wallet_db
+        .fetch_by_id(&(ctx.tx_id as i32))
+        .await
+        .into_diagnostic()?
+        .ok_or(miette::miette!("transaction doesn't exist"))?;
+
+    match record.status {
+        Status::Staging => bail!("transaction must be built before modifying signatures"),
+        _ => (),
+    }
+
+    let mut built_tx: BuiltTransaction =
+        serde_json::from_slice(&record.tx_json).into_diagnostic()?;
+
+    built_tx = built_tx
+        .add_signature(public_key.into(), signature)
+        .into_diagnostic()?;
+
+    // update db
+
+    record.status = Status::Signed;
+    record.tx_json = serde_json::to_vec(&built_tx).into_diagnostic()?;
+    record.tx_cbor = Some(built_tx.tx_bytes.0);
+
+    wallet_db
+        .update_transaction(record)
+        .await
+        .into_diagnostic()?;
+
+    info!("signature added to transaction");
+
+    Ok(())
 }
--- src/transaction/edit/remove_signature.rs
+++ src/transaction/edit/remove_signature.rs
@@ -1,33 +1,72 @@
 use clap::Parser;
-// use miette::{Context, IntoDiagnostic};
-use tracing::instrument;
+use miette::{bail, miette, Context, IntoDiagnostic};
+use pallas::txbuilder::BuiltTransaction;
+use tracing::{info, instrument};
 
-// use super::common::with_staging_tx;
+use crate::wallet::{
+    config::Wallet,
+    dal::{entities::transaction::Status, WalletDB},
+};
 
 #[derive(Parser)]
 pub struct Args {
-    /// public key hash
+    /// hex public key (not pub key hash)
     public_key: String,
 }
 
 #[instrument("remove signature", skip_all, fields(args))]
-pub async fn run(_args: Args, _ctx: &super::EditContext<'_>) -> miette::Result<()> {
-    // TODO should operate on built tx not staging
-    todo!();
+pub async fn run(args: Args, ctx: &super::EditContext<'_>) -> miette::Result<()> {
+    let public_key: [u8; 32] = hex::decode(args.public_key)
+        .into_diagnostic()
+        .context("parsing public key hex")?
+        .try_into()
+        .map_err(|_| miette!("public key incorrect length"))?;
 
-    // let public_key: PublicKey = hex::decode(args.public_key)
-    //     .into_diagnostic()
-    //     .context("parsing public key hex")?
-    //     .into();
+    let wallet = Wallet::load_config(&ctx.global_ctx.dirs.root_dir, &ctx.wallet)?
+        .ok_or(miette::miette!("wallet doesn't exist"))?;
 
-    // with_staging_tx(ctx, move |mut tx| {
-    //     if let Some(mut signatures) = tx.signatures {
-    //         signatures.remove(&public_key);
+    let wallet_db = WalletDB::open(
+        &wallet.name,
+        &Wallet::dir(&ctx.global_ctx.dirs.root_dir, &wallet.name),
+    )
+    .await
+    .into_diagnostic()?;
 
-    //         tx.signatures = (!signatures.is_empty()).then_some(signatures);
-    //     }
+    let mut record = wallet_db
+        .fetch_by_id(&(ctx.tx_id as i32))
+        .await
+        .into_diagnostic()?
+        .ok_or(miette::miette!("transaction doesn't exist"))?;
 
-    //     Ok(tx)
-    // })
-    // .await
+    match record.status {
+        Status::Staging => bail!("transaction must be built before modifying signatures"),
+        _ => (),
+    }
+
+    let mut built_tx: BuiltTransaction =
+        serde_json::from_slice(&record.tx_json).into_diagnostic()?;
+
+    built_tx = built_tx
+        .remove_signature(public_key.into())
+        .into_diagnostic()?;
+
+    // update db
+
+    if built_tx.signatures.as_ref().is_some_and(|x| !x.is_empty()) {
+        record.status = Status::Signed;
+    } else {
+        record.status = Status::Built;
+    }
+
+    record.tx_json = serde_json::to_vec(&built_tx).into_diagnostic()?;
+    record.tx_cbor = Some(built_tx.tx_bytes.0);
+
+    wallet_db
+        .update_transaction(record)
+        .await
+        .into_diagnostic()?;
+
+    info!("removed signature from transaction");
+
+    Ok(())
 }
