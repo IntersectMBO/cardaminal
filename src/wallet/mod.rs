22c1c75b23711750dc4d73a750a1f462f8dea366
Santiago Carmuega <santiago@carmuega.me>
2023-11-27 11:56:18 -0300

    feat: add command to print wallet address

--- src/wallet/mod.rs
+++ src/wallet/mod.rs
@@ -1,6 +1,7 @@
 use clap::{Parser, Subcommand};
 use tracing::instrument;
 
+mod address;
 mod attach;
 mod balance;
 pub mod config;
@@ -28,6 +29,8 @@ enum Commands {
     Create(create::Args),
     /// show wallet info
     Info(info::Args),
+    /// show wallet address
+    Address(address::Args),
     /// list available wallets
     List(list::Args),
     /// update wallet state from chain
@@ -50,6 +53,7 @@ enum Commands {
 pub async fn run(args: Args, ctx: &crate::Context) -> miette::Result<()> {
     match args.command {
         Commands::Create(args) => create::run(args, ctx).await,
+        Commands::Address(args) => address::run(args, ctx).await,
         Commands::Info(args) => info::run(args, ctx).await,
         Commands::List(args) => list::run(args, ctx).await,
         Commands::Update(args) => {
